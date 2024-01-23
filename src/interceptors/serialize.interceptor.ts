d50e7693c56a59f8d82596da5ccc59728d7a26d7
kemal can <kcsari@newmind.ai>
2024-01-23 22:35:14 +0300

    custom interceptor is added

---
+++ src/interceptors/serialize.interceptor.ts
@@ -0,0 +1,32 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  NestInterceptor,
+  UseInterceptors,
+} from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
+import { Observable, map } from 'rxjs';
+
+interface ClassConstructor {
+  new (...args: any[]): object;
+}
+
+export function Serialize(dto: ClassConstructor) {
+  return UseInterceptors(new SerializeInterceptor(dto));
+}
+
+export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: ClassConstructor) {}
+  intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
+    // run something before a request is handled by the request handler
+
+    return handler.handle().pipe(
+      map((data: unknown) => {
+        // Run something before the response is sent out
+        return plainToInstance(this.dto, data, {
+          excludeExtraneousValues: true,
+        });
+      }),
+    );
+  }
+}
